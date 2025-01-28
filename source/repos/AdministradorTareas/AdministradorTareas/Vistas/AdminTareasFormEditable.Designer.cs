0dceff787830714028b9ceb84ffdb6bc1aee4ddb
jmendez1989 <152710433+jmendez1989@users.noreply.github.com>
2025-01-28 15:28:35 -0600

    Update AdminTareasFormEditable.Designer.cs
    Se le colocan los metodos para los botones y los textos de los placeholders

--- source/repos/AdministradorTareas/AdministradorTareas/Vistas/AdminTareasFormEditable.Designer.cs
+++ source/repos/AdministradorTareas/AdministradorTareas/Vistas/AdminTareasFormEditable.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace AdministradorTareas
+namespace AdministradorTareas
 {
     partial class AdminTareasFormEditable
     {
@@ -28,117 +28,173 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lblDescription = new System.Windows.Forms.Label();
+            this.txtDescription = new System.Windows.Forms.TextBox();
+            this.lblUsuario = new System.Windows.Forms.Label();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.lblEstado = new System.Windows.Forms.Label();
+            this.cmbEstado = new System.Windows.Forms.ComboBox();
+            this.lblPrioridad = new System.Windows.Forms.Label();
+            this.cmbPrioridad = new System.Windows.Forms.ComboBox();
+            this.lblFechaCompromiso = new System.Windows.Forms.Label();
+            this.dtpFechaCompromiso = new System.Windows.Forms.DateTimePicker();
+            this.lblNotas = new System.Windows.Forms.Label();
+            this.txtNotas = new System.Windows.Forms.TextBox();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
             this.SuspendLayout();
-
-            // Configuración de la ventana
-            this.ClientSize = new System.Drawing.Size(500, 400);
-            this.Name = "AdminTareasFormEditable";
-            this.Text = "Gestión de Tareas";
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            // 
+            // lblDescription
+            // 
+            this.lblDescription.AutoSize = true;
+            this.lblDescription.Location = new System.Drawing.Point(20, 20);
+            this.lblDescription.Name = "lblDescription";
+            this.lblDescription.Size = new System.Drawing.Size(72, 15);
+            this.lblDescription.TabIndex = 0;
+            this.lblDescription.Text = "Descripción:";
+            // 
+            // txtDescription
+            // 
+            this.txtDescription.Location = new System.Drawing.Point(120, 20);
+            this.txtDescription.Name = "txtDescription";
+            this.txtDescription.Size = new System.Drawing.Size(340, 23);
+            this.txtDescription.TabIndex = 1;
+            this.txtDescription.Enter += new System.EventHandler(this.txtDescription_Enter);
+            this.txtDescription.Leave += new System.EventHandler(this.txtDescription_Leave);
+            // 
+            // lblUsuario
+            // 
+            this.lblUsuario.AutoSize = true;
+            this.lblUsuario.Location = new System.Drawing.Point(20, 60);
+            this.lblUsuario.Name = "lblUsuario";
+            this.lblUsuario.Size = new System.Drawing.Size(50, 15);
+            this.lblUsuario.TabIndex = 2;
+            this.lblUsuario.Text = "Usuario:";
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Location = new System.Drawing.Point(120, 60);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(340, 23);
+            this.txtUsuario.TabIndex = 3;
+            this.txtUsuario.Enter += new System.EventHandler(this.txtUsuario_Enter);
+            this.txtUsuario.Leave += new System.EventHandler(this.txtUsuario_Leave);
+            // 
+            // lblEstado
+            // 
+            this.lblEstado.AutoSize = true;
+            this.lblEstado.Location = new System.Drawing.Point(20, 100);
+            this.lblEstado.Name = "lblEstado";
+            this.lblEstado.Size = new System.Drawing.Size(45, 15);
+            this.lblEstado.TabIndex = 4;
+            this.lblEstado.Text = "Estado:";
+            // 
+            // cmbEstado
+            // 
+            this.cmbEstado.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbEstado.Location = new System.Drawing.Point(120, 100);
+            this.cmbEstado.Name = "cmbEstado";
+            this.cmbEstado.Size = new System.Drawing.Size(340, 23);
+            this.cmbEstado.TabIndex = 5;
+            // 
+            // lblPrioridad
+            // 
+            this.lblPrioridad.AutoSize = true;
+            this.lblPrioridad.Location = new System.Drawing.Point(20, 140);
+            this.lblPrioridad.Name = "lblPrioridad";
+            this.lblPrioridad.Size = new System.Drawing.Size(58, 15);
+            this.lblPrioridad.TabIndex = 6;
+            this.lblPrioridad.Text = "Prioridad:";
+            // 
+            // cmbPrioridad
+            // 
+            this.cmbPrioridad.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbPrioridad.Location = new System.Drawing.Point(120, 140);
+            this.cmbPrioridad.Name = "cmbPrioridad";
+            this.cmbPrioridad.Size = new System.Drawing.Size(340, 23);
+            this.cmbPrioridad.TabIndex = 7;
+            // 
+            // lblFechaCompromiso
+            // 
+            this.lblFechaCompromiso.AutoSize = true;
+            this.lblFechaCompromiso.Location = new System.Drawing.Point(20, 180);
+            this.lblFechaCompromiso.Name = "lblFechaCompromiso";
+            this.lblFechaCompromiso.Size = new System.Drawing.Size(77, 15);
+            this.lblFechaCompromiso.TabIndex = 8;
+            this.lblFechaCompromiso.Text = "Fecha Límite:";
+            // 
+            // dtpFechaCompromiso
+            // 
+            this.dtpFechaCompromiso.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpFechaCompromiso.Location = new System.Drawing.Point(120, 180);
+            this.dtpFechaCompromiso.Name = "dtpFechaCompromiso";
+            this.dtpFechaCompromiso.Size = new System.Drawing.Size(340, 23);
+            this.dtpFechaCompromiso.TabIndex = 9;
+            // 
+            // lblNotas
+            // 
+            this.lblNotas.AutoSize = true;
+            this.lblNotas.Location = new System.Drawing.Point(20, 220);
+            this.lblNotas.Name = "lblNotas";
+            this.lblNotas.Size = new System.Drawing.Size(41, 15);
+            this.lblNotas.TabIndex = 10;
+            this.lblNotas.Text = "Notas:";
+            // 
+            // txtNotas
+            // 
+            this.txtNotas.Location = new System.Drawing.Point(120, 220);
+            this.txtNotas.Multiline = true;
+            this.txtNotas.Name = "txtNotas";
+            this.txtNotas.Size = new System.Drawing.Size(340, 80);
+            this.txtNotas.TabIndex = 11;
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.BackColor = System.Drawing.Color.LightGreen;
+            this.btnGuardar.Location = new System.Drawing.Point(120, 320);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(100, 30);
+            this.btnGuardar.TabIndex = 12;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = false;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.BackColor = System.Drawing.Color.LightCoral;
+            this.btnCancelar.Location = new System.Drawing.Point(260, 320);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(100, 30);
+            this.btnCancelar.TabIndex = 13;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = false;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // AdminTareasFormEditable
+            // 
             this.BackColor = System.Drawing.Color.WhiteSmoke;
-
-            // Etiqueta Descripción
-            var lblDescription = new System.Windows.Forms.Label();
-            lblDescription.Text = "Descripción:";
-            lblDescription.Location = new System.Drawing.Point(20, 20);
-            lblDescription.AutoSize = true;
-            this.Controls.Add(lblDescription);
-
-            // Caja de texto Descripción
-            var txtDescription = new System.Windows.Forms.TextBox();
-            txtDescription.Location = new System.Drawing.Point(120, 20);
-            txtDescription.Size = new System.Drawing.Size(340, 23);
-            this.Controls.Add(txtDescription);
-
-            // Etiqueta Usuario
-            var lblUsuario = new System.Windows.Forms.Label();
-            lblUsuario.Text = "Usuario:";
-            lblUsuario.Location = new System.Drawing.Point(20, 60);
-            lblUsuario.AutoSize = true;
-            this.Controls.Add(lblUsuario);
-
-            // Caja de texto Usuario
-            var txtUsuario = new System.Windows.Forms.TextBox();
-            txtUsuario.Location = new System.Drawing.Point(120, 60);
-            txtUsuario.Size = new System.Drawing.Size(340, 23);
-            this.Controls.Add(txtUsuario);
-
-            // Etiqueta Estado
-            var lblEstado = new System.Windows.Forms.Label();
-            lblEstado.Text = "Estado:";
-            lblEstado.Location = new System.Drawing.Point(20, 100);
-            lblEstado.AutoSize = true;
-            this.Controls.Add(lblEstado);
-
-            // ComboBox Estado
-            var cmbEstado = new System.Windows.Forms.ComboBox();
-            cmbEstado.Location = new System.Drawing.Point(120, 100);
-            cmbEstado.Size = new System.Drawing.Size(340, 23);
-            cmbEstado.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.Controls.Add(cmbEstado);
-
-            // Etiqueta Prioridad
-            var lblPrioridad = new System.Windows.Forms.Label();
-            lblPrioridad.Text = "Prioridad:";
-            lblPrioridad.Location = new System.Drawing.Point(20, 140);
-            lblPrioridad.AutoSize = true;
-            this.Controls.Add(lblPrioridad);
-
-            // ComboBox Prioridad
-            var cmbPrioridad = new System.Windows.Forms.ComboBox();
-            cmbPrioridad.Location = new System.Drawing.Point(120, 140);
-            cmbPrioridad.Size = new System.Drawing.Size(340, 23);
-            cmbPrioridad.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.Controls.Add(cmbPrioridad);
-
-            // Etiqueta Fecha Límite
-            var lblFechaCompromiso = new System.Windows.Forms.Label();
-            lblFechaCompromiso.Text = "Fecha Límite:";
-            lblFechaCompromiso.Location = new System.Drawing.Point(20, 180);
-            lblFechaCompromiso.AutoSize = true;
-            this.Controls.Add(lblFechaCompromiso);
-
-            // DateTimePicker Fecha Límite
-            var dtpFechaCompromiso = new System.Windows.Forms.DateTimePicker();
-            dtpFechaCompromiso.Location = new System.Drawing.Point(120, 180);
-            dtpFechaCompromiso.Size = new System.Drawing.Size(340, 23);
-            dtpFechaCompromiso.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.Controls.Add(dtpFechaCompromiso);
-
-            // Etiqueta Notas
-            var lblNotas = new System.Windows.Forms.Label();
-            lblNotas.Text = "Notas:";
-            lblNotas.Location = new System.Drawing.Point(20, 220);
-            lblNotas.AutoSize = true;
-            this.Controls.Add(lblNotas);
-
-            // Caja de texto Notas
-            var txtNotas = new System.Windows.Forms.TextBox();
-            txtNotas.Location = new System.Drawing.Point(120, 220);
-            txtNotas.Size = new System.Drawing.Size(340, 80);
-            txtNotas.Multiline = true;
-            this.Controls.Add(txtNotas);
-
-            // Botón Guardar
-            var btnGuardar = new System.Windows.Forms.Button();
-            btnGuardar.Text = "Guardar";
-            btnGuardar.Location = new System.Drawing.Point(120, 320);
-            btnGuardar.Size = new System.Drawing.Size(100, 30);
-            btnGuardar.BackColor = System.Drawing.Color.LightGreen;
-            btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
-            this.Controls.Add(btnGuardar);
-
-            // Botón Cancelar
-            var btnCancelar = new System.Windows.Forms.Button();
-            btnCancelar.Text = "Cancelar";
-            btnCancelar.Location = new System.Drawing.Point(260, 320);
-            btnCancelar.Size = new System.Drawing.Size(100, 30);
-            btnCancelar.BackColor = System.Drawing.Color.LightCoral;
-            btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
-            this.Controls.Add(btnCancelar);
-
+            this.ClientSize = new System.Drawing.Size(500, 400);
+            this.Controls.Add(this.lblDescription);
+            this.Controls.Add(this.txtDescription);
+            this.Controls.Add(this.lblUsuario);
+            this.Controls.Add(this.txtUsuario);
+            this.Controls.Add(this.lblEstado);
+            this.Controls.Add(this.cmbEstado);
+            this.Controls.Add(this.lblPrioridad);
+            this.Controls.Add(this.cmbPrioridad);
+            this.Controls.Add(this.lblFechaCompromiso);
+            this.Controls.Add(this.dtpFechaCompromiso);
+            this.Controls.Add(this.lblNotas);
+            this.Controls.Add(this.txtNotas);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.btnCancelar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "AdminTareasFormEditable";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Gestión de Tareas";
             this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         private System.Windows.Forms.Label lblDescription;
