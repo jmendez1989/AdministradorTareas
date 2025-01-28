a1f211065006c2a92ed2ff4deaf2dc674607e572
jmendez1989 <152710433+jmendez1989@users.noreply.github.com>
2025-01-28 13:11:15 -0600

    Update TablaAdminTareas.Designer.cs
    Se renueva el aspecto visual para darle un efecto más limpio y más amigable con el usuario.

--- source/repos/AdministradorTareas/AdministradorTareas/Vistas/TablaAdminTareas.Designer.cs
+++ source/repos/AdministradorTareas/AdministradorTareas/Vistas/TablaAdminTareas.Designer.cs
@@ -23,8 +23,9 @@
         ///  Required method for Designer support - do not modify
         ///  the contents of this method with the code editor.
         /// </summary>
-        private void InitializeComponent()
+               private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             this.dataGridViewTareas = new System.Windows.Forms.DataGridView();
             this.btnAgregar = new System.Windows.Forms.Button();
             this.btnEditar = new System.Windows.Forms.Button();
@@ -42,150 +43,146 @@
             // 
             // dataGridViewTareas
             // 
-            this.dataGridViewTareas.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridViewTareas.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.dataGridViewTareas.BackgroundColor = System.Drawing.Color.WhiteSmoke;
             this.dataGridViewTareas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridViewTareas.Location = new System.Drawing.Point(14, 58);
-            this.dataGridViewTareas.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridViewTareas.DefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridViewTareas.Location = new System.Drawing.Point(20, 80);
             this.dataGridViewTareas.Name = "dataGridViewTareas";
-            this.dataGridViewTareas.ReadOnly = true;
-            this.dataGridViewTareas.Size = new System.Drawing.Size(947, 404);
+            this.dataGridViewTareas.Size = new System.Drawing.Size(900, 350);
             this.dataGridViewTareas.TabIndex = 0;
-            this.dataGridViewTareas.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewTareas_CellClick);
             // 
             // btnAgregar
             // 
-            this.btnAgregar.Location = new System.Drawing.Point(14, 479);
-            this.btnAgregar.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btnAgregar.BackColor = System.Drawing.Color.LightGreen;
+            this.btnAgregar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAgregar.Location = new System.Drawing.Point(20, 450);
             this.btnAgregar.Name = "btnAgregar";
-            this.btnAgregar.Size = new System.Drawing.Size(88, 27);
+            this.btnAgregar.Size = new System.Drawing.Size(100, 30);
             this.btnAgregar.TabIndex = 1;
             this.btnAgregar.Text = "Agregar";
-            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.UseVisualStyleBackColor = false;
             this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
             // 
             // btnEditar
             // 
-            this.btnEditar.Location = new System.Drawing.Point(108, 479);
-            this.btnEditar.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btnEditar.BackColor = System.Drawing.Color.LightBlue;
+            this.btnEditar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEditar.Location = new System.Drawing.Point(130, 450);
             this.btnEditar.Name = "btnEditar";
-            this.btnEditar.Size = new System.Drawing.Size(88, 27);
+            this.btnEditar.Size = new System.Drawing.Size(100, 30);
             this.btnEditar.TabIndex = 2;
             this.btnEditar.Text = "Editar";
-            this.btnEditar.UseVisualStyleBackColor = true;
+            this.btnEditar.UseVisualStyleBackColor = false;
             this.btnEditar.Click += new System.EventHandler(this.btnEditar_Click);
             // 
             // btnBorrar
             // 
-            this.btnBorrar.Location = new System.Drawing.Point(203, 479);
-            this.btnBorrar.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btnBorrar.BackColor = System.Drawing.Color.LightCoral;
+            this.btnBorrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBorrar.Location = new System.Drawing.Point(240, 450);
             this.btnBorrar.Name = "btnBorrar";
-            this.btnBorrar.Size = new System.Drawing.Size(88, 27);
+            this.btnBorrar.Size = new System.Drawing.Size(100, 30);
             this.btnBorrar.TabIndex = 3;
             this.btnBorrar.Text = "Eliminar";
-            this.btnBorrar.UseVisualStyleBackColor = true;
+            this.btnBorrar.UseVisualStyleBackColor = false;
             this.btnBorrar.Click += new System.EventHandler(this.btnBorrar_Click);
             // 
             // lblFiltro
             // 
-            this.lblFiltro.AutoSize = true;
-            this.lblFiltro.Location = new System.Drawing.Point(14, 17);
-            this.lblFiltro.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblFiltro.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblFiltro.Location = new System.Drawing.Point(20, 20);
             this.lblFiltro.Name = "lblFiltro";
-            this.lblFiltro.Size = new System.Drawing.Size(42, 15);
+            this.lblFiltro.Size = new System.Drawing.Size(56, 23);
             this.lblFiltro.TabIndex = 4;
-            this.lblFiltro.Text = "Filtros:";
+            this.lblFiltro.Text = "Filtros";
             // 
             // txtFiltroUsuario
             // 
-            this.txtFiltroUsuario.Location = new System.Drawing.Point(70, 14);
-            this.txtFiltroUsuario.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.txtFiltroUsuario.Location = new System.Drawing.Point(82, 20);
             this.txtFiltroUsuario.Name = "txtFiltroUsuario";
-            this.txtFiltroUsuario.Size = new System.Drawing.Size(140, 23);
+            this.txtFiltroUsuario.Size = new System.Drawing.Size(119, 23);
             this.txtFiltroUsuario.TabIndex = 5;
             this.txtFiltroUsuario.Text = "Usuario";
             // 
             // cmbFiltroEstado
             // 
-            this.cmbFiltroEstado.FormattingEnabled = true;
-            this.cmbFiltroEstado.Location = new System.Drawing.Point(218, 14);
-            this.cmbFiltroEstado.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.cmbFiltroEstado.Location = new System.Drawing.Point(210, 20);
             this.cmbFiltroEstado.Name = "cmbFiltroEstado";
-            this.cmbFiltroEstado.Size = new System.Drawing.Size(140, 23);
+            this.cmbFiltroEstado.Size = new System.Drawing.Size(130, 23);
             this.cmbFiltroEstado.TabIndex = 6;
             this.cmbFiltroEstado.Text = "Estado";
             // 
             // cmbFiltroPrioridad
             // 
-            this.cmbFiltroPrioridad.FormattingEnabled = true;
-            this.cmbFiltroPrioridad.Location = new System.Drawing.Point(366, 14);
-            this.cmbFiltroPrioridad.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.cmbFiltroPrioridad.Location = new System.Drawing.Point(346, 20);
             this.cmbFiltroPrioridad.Name = "cmbFiltroPrioridad";
-            this.cmbFiltroPrioridad.Size = new System.Drawing.Size(140, 23);
+            this.cmbFiltroPrioridad.Size = new System.Drawing.Size(130, 23);
             this.cmbFiltroPrioridad.TabIndex = 7;
             this.cmbFiltroPrioridad.Text = "Prioridad";
             // 
             // dtpFechaDesde
             // 
             this.dtpFechaDesde.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.dtpFechaDesde.Location = new System.Drawing.Point(514, 14);
-            this.dtpFechaDesde.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.dtpFechaDesde.Location = new System.Drawing.Point(482, 20);
             this.dtpFechaDesde.Name = "dtpFechaDesde";
-            this.dtpFechaDesde.Size = new System.Drawing.Size(116, 23);
+            this.dtpFechaDesde.Size = new System.Drawing.Size(136, 23);
             this.dtpFechaDesde.TabIndex = 8;
             // 
             // dtpFechaHasta
             // 
             this.dtpFechaHasta.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.dtpFechaHasta.Location = new System.Drawing.Point(638, 14);
-            this.dtpFechaHasta.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.dtpFechaHasta.Location = new System.Drawing.Point(624, 20);
             this.dtpFechaHasta.Name = "dtpFechaHasta";
-            this.dtpFechaHasta.Size = new System.Drawing.Size(116, 23);
+            this.dtpFechaHasta.Size = new System.Drawing.Size(126, 23);
             this.dtpFechaHasta.TabIndex = 9;
             // 
             // btnAplicarFiltros
             // 
-            this.btnAplicarFiltros.Location = new System.Drawing.Point(762, 12);
-            this.btnAplicarFiltros.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btnAplicarFiltros.BackColor = System.Drawing.Color.LightGray;
+            this.btnAplicarFiltros.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAplicarFiltros.Location = new System.Drawing.Point(760, 20);
             this.btnAplicarFiltros.Name = "btnAplicarFiltros";
-            this.btnAplicarFiltros.Size = new System.Drawing.Size(95, 27);
+            this.btnAplicarFiltros.Size = new System.Drawing.Size(100, 25);
             this.btnAplicarFiltros.TabIndex = 10;
-            this.btnAplicarFiltros.Text = "Aplicar Filtros";
-            this.btnAplicarFiltros.UseVisualStyleBackColor = true;
+            this.btnAplicarFiltros.Text = "Aplicar";
+            this.btnAplicarFiltros.UseVisualStyleBackColor = false;
             this.btnAplicarFiltros.Click += new System.EventHandler(this.btnAplicarFiltros_Click);
             // 
             // btnLimpiarFiltros
             // 
-            this.btnLimpiarFiltros.Location = new System.Drawing.Point(865, 11);
-            this.btnLimpiarFiltros.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btnLimpiarFiltros.BackColor = System.Drawing.Color.LightGray;
+            this.btnLimpiarFiltros.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLimpiarFiltros.Location = new System.Drawing.Point(870, 20);
             this.btnLimpiarFiltros.Name = "btnLimpiarFiltros";
-            this.btnLimpiarFiltros.Size = new System.Drawing.Size(98, 27);
+            this.btnLimpiarFiltros.Size = new System.Drawing.Size(100, 25);
             this.btnLimpiarFiltros.TabIndex = 11;
-            this.btnLimpiarFiltros.Text = "Limpiar Filtros";
-            this.btnLimpiarFiltros.UseVisualStyleBackColor = true;
+            this.btnLimpiarFiltros.Text = "Limpiar";
+            this.btnLimpiarFiltros.UseVisualStyleBackColor = false;
             this.btnLimpiarFiltros.Click += new System.EventHandler(this.btnLimpiarFiltros_Click);
             // 
             // TablaAdminTareas
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(975, 532);
-            this.Controls.Add(this.btnLimpiarFiltros);
-            this.Controls.Add(this.btnAplicarFiltros);
-            this.Controls.Add(this.dtpFechaHasta);
-            this.Controls.Add(this.dtpFechaDesde);
-            this.Controls.Add(this.cmbFiltroPrioridad);
-            this.Controls.Add(this.cmbFiltroEstado);
-            this.Controls.Add(this.txtFiltroUsuario);
-            this.Controls.Add(this.lblFiltro);
-            this.Controls.Add(this.btnBorrar);
-            this.Controls.Add(this.btnEditar);
-            this.Controls.Add(this.btnAgregar);
+            this.ClientSize = new System.Drawing.Size(1000, 500);
             this.Controls.Add(this.dataGridViewTareas);
-            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
-            this.MaximizeBox = false;
-            this.MinimizeBox = false;
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.btnEditar);
+            this.Controls.Add(this.btnBorrar);
+            this.Controls.Add(this.lblFiltro);
+            this.Controls.Add(this.txtFiltroUsuario);
+            this.Controls.Add(this.cmbFiltroEstado);
+            this.Controls.Add(this.cmbFiltroPrioridad);
+            this.Controls.Add(this.dtpFechaDesde);
+            this.Controls.Add(this.dtpFechaHasta);
+            this.Controls.Add(this.btnAplicarFiltros);
+            this.Controls.Add(this.btnLimpiarFiltros);
             this.Name = "TablaAdminTareas";
             this.Text = "Administrador de Tareas";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewTareas)).EndInit();
